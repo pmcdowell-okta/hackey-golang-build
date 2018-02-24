10ae39ecc60a14ba83f9118180cfe9a3bdd91543
pmcdowell-okta <pmcdowell@okta.com>
2018-02-24 00:01:54 -0800

    Update main.go

--- src/main/main.go
+++ src/main/main.go
@@ -6,26 +6,46 @@ import (
 	"net/http"
 	"log"
 	"localhtml"
+	//"os"
+	"github.com/gorilla/mux"
 )
 
+func boom(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("called custom route")
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
+}
 
-	// This code fetches the html file I want
-	data, err := localhtml.Asset("index.html")
+func catchAllHtml(w http.ResponseWriter, r *http.Request) {
+
+	// This code fetches the html file I want. Pulling from Assets that are compiled in
+	data, err := localhtml.Asset(r.RequestURI[1:])
 	if err != nil {
 		// Asset was not found.
+		logwrapper("File not found: "+r.RequestURI[1:])
+	} else {
+		fmt.Fprintf(w, string(data)) // send data to client side
 	}
-
-
-	fmt.Fprintf(w, string(data)) // send data to client side
 }
 
 
 func main() {
-	http.HandleFunc("/", sayhelloName) // set router
+
+	r := mux.NewRouter()
+
+	//You can put specific routes in here
+	r.HandleFunc("/customroute", boom)
+
+	//If no other route is found, then look in the Asset's build in
+	r.PathPrefix("/").HandlerFunc(catchAllHtml)
+
+	http.Handle("/", r)
+
 	err := http.ListenAndServe(":3000", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func logwrapper ( logline string ) {
+	fmt.Println(logline)
 }
